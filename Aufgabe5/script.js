9dda97e89763c817e0d6323e97c3343969357fb9
Mariia Kolkutova <kolkutova.mariya@gmail.com>
2020-11-20 18:26:34 +0100

    emission all coutries

--- Aufgabe5/script.js
+++ Aufgabe5/script.js
@@ -1,1 +1,14 @@
+var africa2008 = 1028;
+var africa2018 = 1235.5;
+var southAmerica2008 = 1132.6;
+var southAmerica2018 = 1261.5;
+var europe2008 = 4965.7;
+var europe2018 = 4209.3;
+var northAmerica2008 = 6600.4;
+var northAmerica2018 = 6035.6;
+var asia2008 = 12954.7;
+var asia2018 = 16274.1;
+var australia2008 = 1993;
+var australia2018 = 2100.5;
+var emissionInsgesamt2018 = africa2018 + southAmerica2018 + europe2018 + northAmerica2018 + asia2018 + australia2018;
 //# sourceMappingURL=script.js.map
---
+++ Aufgabe5/script.ts
@@ -0,0 +1,19 @@
+var africa2008: number = 1028;
+var africa2018: number = 1235.5;
+
+var southAmerica2008: number = 1132.6;
+var southAmerica2018: number = 1261.5;
+
+var europe2008: number = 4965.7;
+var europe2018: number = 4209.3;
+
+var northAmerica2008: number = 6600.4;
+var northAmerica2018: number = 6035.6;
+
+var asia2008: number = 12954.7;
+var asia2018: number = 16274.1;
+
+var australia2008: number = 1993;
+var australia2018: number = 2100.5;
+
+var emissionInsgesamt2018: number = africa2018 + southAmerica2018 + europe2018 + northAmerica2018 + asia2018 + australia2018
