19466ced6f95a0ef216be151585eb567ae30dd57
Mariia Kolkutova <kolkutova.mariya@gmail.com>
2020-11-21 12:42:49 +0100

    em

--- Aufgabe5/script.js
+++ Aufgabe5/script.js
@@ -1,3 +1,4 @@
+//Emission aller Kontinente 2008 & 2018
 console.log("Worldwide Emission");
 var africa2008 = 1028;
 var africa2018 = 1235.5;
@@ -11,5 +12,10 @@ var asia2008 = 12954.7;
 var asia2018 = 16274.1;
 var australia2008 = 1993;
 var australia2018 = 2100.5;
+//Gesamtemission aller Kontinente 2018 
 var emissionInsgesamt2018 = (africa2018 + southAmerica2018 + europe2018 + northAmerica2018 + asia2018 + australia2018);
+//Gesamtemission aller Kontinete 2008
+var emissionInsgesamt2008 = (africa2008 + southAmerica2008 + europe2008 + northAmerica2008 + asia2008 + australia2008);
+//Africa
+console.log("Die Emission von Africa ist:' kg CO2");
 //# sourceMappingURL=script.js.map
--- Aufgabe5/script.ts
+++ Aufgabe5/script.ts
@@ -1,3 +1,4 @@
+//Emission aller Kontinente 2008 & 2018
 console.log("Worldwide Emission");
 var africa2008: number = 1028;
 var africa2018: number = 1235.5;
@@ -17,4 +18,10 @@ var asia2018: number = 16274.1;
 var australia2008: number = 1993;
 var australia2018: number = 2100.5;
 
+//Gesamtemission aller Kontinente 2018 
 var emissionInsgesamt2018: number = (africa2018 + southAmerica2018 + europe2018 + northAmerica2018 + asia2018 + australia2018);
+//Gesamtemission aller Kontinete 2008
+var emissionInsgesamt2008: number = (africa2008 + southAmerica2008 + europe2008 + northAmerica2008 + asia2008 + australia2008);
+
+//Africa
+console.log("Die Emission von Africa ist:' kg CO2")
