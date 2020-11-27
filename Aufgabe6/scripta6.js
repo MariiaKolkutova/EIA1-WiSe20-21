897968481be3f6088a6e86aa9a3e7a0cb67e2ce5
Mariia Kolkutova <kolkutova.mariya@gmail.com>
2020-11-27 11:25:12 +0100

    a6

--- Aufgabe6/scripta6.js
+++ Aufgabe6/scripta6.js
@@ -1,1 +1,58 @@
+//Emission aller Kontinente 2008 & 2018
+var aufgabe6;
+(function (aufgabe6) {
+    var africa2008 = 1028;
+    var africa2018 = 1235.5;
+    var southAmerica2008 = 1132.6;
+    var southAmerica2018 = 1261.5;
+    var europe2008 = 4965.7;
+    var europe2018 = 4209.3;
+    var northAmerica2008 = 6600.4;
+    var northAmerica2018 = 6035.6;
+    var asia2008 = 12954.7;
+    var asia2018 = 16274.1;
+    var australia2008 = 1993;
+    var australia2018 = 2100.5;
+    //Gesamtemission aller Kontinente 2018 
+    var emissionInsgesamt2018 = (africa2018 + southAmerica2018 + europe2018 + northAmerica2018 + asia2018 + australia2018);
+    //Gesamtemission aller Kontinete 2008
+    var emissionInsgesamt2008 = (africa2008 + southAmerica2008 + europe2008 + northAmerica2008 + asia2008 + australia2008);
+    //Prozentanteil der Gesamtemission aller Kontinente 2018
+    var prozentanteilAfrica18 = ((africa2018 / emissionInsgesamt2018) * 100);
+    var prozentanteilSouthAmerica18 = ((southAmerica2018 / emissionInsgesamt2018) * 100);
+    var prozentanteilNorthAmerica18 = ((northAmerica2018 / emissionInsgesamt2018) * 100);
+    var prozentanteilEurope18 = ((europe2018 / emissionInsgesamt2018) * 100);
+    var prozentanteilAsia18 = ((asia2018 / emissionInsgesamt2018) * 100);
+    var prozentanteilAustralia18 = ((australia2018 / emissionInsgesamt2018) * 100);
+    //Prozentanteil der Gesamtemission aller Kontinente 2008
+    var prozentanteilAfrica08 = ((africa2008 / emissionInsgesamt2008) * 100);
+    var prozentanteilSouthAmerica08 = ((southAmerica2008 / emissionInsgesamt2008) * 100);
+    var prozentanteilNorthAmerica08 = ((northAmerica2008 / emissionInsgesamt2008) * 100);
+    var prozentanteilEurope08 = ((europe2008 / emissionInsgesamt2008) * 100);
+    var prozentanteilAsia08 = ((asia2008 / emissionInsgesamt2008) * 100);
+    var prozentanteilAustralia08 = ((australia2008 / emissionInsgesamt2008) * 100);
+    // Prozentuale veränderung der Gesamtemission aller Kontinente 2008 & 2018
+    var prozentveraenderungAfrica = (((prozentanteilAfrica18 / prozentanteilAfrica08) - 1) * 100);
+    var prozentveraenderungSouthAmerica = (((prozentanteilSouthAmerica18 / prozentanteilSouthAmerica08) - 1) * 100);
+    var prozentveraenderungNorthAmerica = (((prozentanteilNorthAmerica18 / prozentanteilNorthAmerica08) - 1) * 100);
+    var prozentveraenderungEurope = (((prozentanteilEurope18 / prozentanteilEurope08) - 1) * 100);
+    var prozentveraenderungAsia = (((prozentanteilAsia18 / prozentanteilAsia08) - 1) * 100);
+    var prozentveraenderungAustralia = (((prozentanteilAustralia18 / prozentanteilAustralia08) - 1) * 100);
+    // (Zahlen) Kontinent Vergleich "world carbon dioxide emission" 2008 und 2018 
+    var ZverglCO2eAfrica = (africa2018 - africa2008);
+    var ZverglCO2eNorthAmerica = (northAmerica2018 - northAmerica2008);
+    var ZverglCO2eSouthAmerica = (southAmerica2018 - southAmerica2008);
+    var ZverglCO2eEurope = (europe2018 - europe2008);
+    var ZverglCO2eAsia = (asia2018 - asia2008);
+    var ZverglCO2eAustralia = (australia2018 - australia2008);
+    // (Prozent) Kontinent Vergleich "world carbon dioxide emission" 2008 und 2018 
+    var PverglCO2eAfrica = (((africa2018 / africa2008) - 1) * 100);
+    var PverglCO2eNorthAmerica = (((northAmerica2018 / northAmerica2008) - 1) * 100);
+    var PverglCO2eSouthAmerica = (((southAmerica2018 / southAmerica2008) - 1) * 100);
+    var PverglCO2eEurope = (((europe2018 / europe2008) - 1) * 100);
+    var PverglCO2eAsia = (((asia2018 / asia2008) - 1) * 100);
+    var PverglCO2eAustralia = (((australia2018 / australia2008) - 1) * 100);
+    //Funktion
+})(aufgabe6 || (aufgabe6 = {}));
+;
 //# sourceMappingURL=scripta6.js.map
---
+++ Aufgabe6/scripta6.ts
@@ -0,0 +1,70 @@
+//Emission aller Kontinente 2008 & 2018
+namespace aufgabe6{ 
+
+    var africa2008: number = 1028;
+    var africa2018: number = 1235.5;
+    
+    var southAmerica2008: number = 1132.6;
+    var southAmerica2018: number = 1261.5;
+    
+    var europe2008: number = 4965.7;
+    var europe2018: number = 4209.3;
+    
+    var northAmerica2008: number = 6600.4;
+    var northAmerica2018: number = 6035.6;
+    
+    var asia2008: number = 12954.7;
+    var asia2018: number = 16274.1;
+    
+    var australia2008: number = 1993;
+    var australia2018: number = 2100.5;
+
+//Gesamtemission aller Kontinente 2018 
+var emissionInsgesamt2018: number = (africa2018 + southAmerica2018 + europe2018 + northAmerica2018 + asia2018 + australia2018);
+
+//Gesamtemission aller Kontinete 2008
+var emissionInsgesamt2008: number = (africa2008 + southAmerica2008 + europe2008 + northAmerica2008 + asia2008 + australia2008);
+
+//Prozentanteil der Gesamtemission aller Kontinente 2018
+var prozentanteilAfrica18 : number = ((africa2018 / emissionInsgesamt2018) * 100);
+var prozentanteilSouthAmerica18 : number = ((southAmerica2018 / emissionInsgesamt2018) * 100);
+var prozentanteilNorthAmerica18 : number = ((northAmerica2018 / emissionInsgesamt2018) * 100);
+var prozentanteilEurope18 : number = ((europe2018 / emissionInsgesamt2018) * 100);
+var prozentanteilAsia18 : number = ((asia2018 / emissionInsgesamt2018) * 100);
+var prozentanteilAustralia18 : number = ((australia2018 / emissionInsgesamt2018) * 100);
+
+//Prozentanteil der Gesamtemission aller Kontinente 2008
+var prozentanteilAfrica08 : number = ((africa2008 / emissionInsgesamt2008) * 100);
+var prozentanteilSouthAmerica08 : number = ((southAmerica2008 / emissionInsgesamt2008) * 100);
+var prozentanteilNorthAmerica08 : number = ((northAmerica2008 / emissionInsgesamt2008) * 100);
+var prozentanteilEurope08 : number = ((europe2008 / emissionInsgesamt2008) * 100);
+var prozentanteilAsia08 : number = ((asia2008 / emissionInsgesamt2008) * 100);
+var prozentanteilAustralia08 : number = ((australia2008 / emissionInsgesamt2008) * 100);
+
+// Prozentuale veränderung der Gesamtemission aller Kontinente 2008 & 2018
+var prozentveraenderungAfrica : number = (((prozentanteilAfrica18 / prozentanteilAfrica08) -1) * 100);
+var prozentveraenderungSouthAmerica : number = (((prozentanteilSouthAmerica18 / prozentanteilSouthAmerica08) -1) * 100);
+var prozentveraenderungNorthAmerica : number = (((prozentanteilNorthAmerica18 / prozentanteilNorthAmerica08) -1) * 100);
+var prozentveraenderungEurope : number = (((prozentanteilEurope18 / prozentanteilEurope08) -1) * 100);
+var prozentveraenderungAsia : number = (((prozentanteilAsia18 / prozentanteilAsia08) -1) * 100);
+var prozentveraenderungAustralia: number = (((prozentanteilAustralia18 / prozentanteilAustralia08) -1) * 100);
+
+// (Zahlen) Kontinent Vergleich "world carbon dioxide emission" 2008 und 2018 
+var ZverglCO2eAfrica : number = (africa2018 - africa2008);
+var ZverglCO2eNorthAmerica : number = (northAmerica2018 - northAmerica2008);
+var ZverglCO2eSouthAmerica : number = (southAmerica2018 - southAmerica2008);
+var ZverglCO2eEurope : number = (europe2018 - europe2008);
+var ZverglCO2eAsia : number = (asia2018 - asia2008);
+var ZverglCO2eAustralia : number = (australia2018 - australia2008);
+
+// (Prozent) Kontinent Vergleich "world carbon dioxide emission" 2008 und 2018 
+var PverglCO2eAfrica : number = (((africa2018 / africa2008) -1) * 100);
+var PverglCO2eNorthAmerica : number = (((northAmerica2018 / northAmerica2008) -1) * 100);
+var PverglCO2eSouthAmerica : number = (((southAmerica2018 / southAmerica2008) -1) * 100);
+var PverglCO2eEurope : number = (((europe2018 / europe2008) -1) * 100);
+var PverglCO2eAsia : number = (((asia2018 / asia2008) -1) * 100);
+var PverglCO2eAustralia : number = (((australia2018 / australia2008) -1) * 100);
+
+//Funktion
+
+};
