9c6c06ef930b6f4ebfb93b0cdd92f7a35ffad397
Mariia Kolkutova <kolkutova.mariya@gmail.com>
2020-12-16 16:45:40 +0100

    ?

--- Aufgabe9/scriptA9.ts
+++ Aufgabe9/scriptA9.ts
@@ -1,10 +1,30 @@
 namespace L9{
 
-interface TodoListItems{
-    todo: {
-      text: string;
-      complete: boolean;
+    let test: number = 0;
+
+interface todo{
+      title: string;
+      done: boolean;
     }
+
+let todos: todo[] = [
+{ 
+title: 'Geschenke',
+done: true, 
+},
+{
+title: 'Glühwein',
+done: true,
+},
+{
+title: 'mehr Alkohol kaufen',
+done: false,
+}
+];
+
+for (let i: number = 0; i< todos.length; i++){
+    document.querySelector(".todoListe ul").innerHTML += "<li>" + todos[i].title + "[" + todos[i].done + "] </li>";
+}
 }
 
 
