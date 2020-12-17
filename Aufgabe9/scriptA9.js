22d9c6366b7767cf2ac3ef216fb2584c6dfbd22d
Mariia Kolkutova <kolkutova.mariya@gmail.com>
2020-12-17 14:47:52 +0100

    ???

--- Aufgabe9/scriptA9.js
+++ Aufgabe9/scriptA9.js
@@ -3,19 +3,21 @@ var L9;
     var inputField = document.getElementById("inputField");
     inputField.addEventListener("keypress", function (event) {
         if (event.key == "Enter") {
-            createNewToDo();
+            createNewTask();
         }
     });
-    function createNewToDo() {
+    function createNewTask() {
         var myCheckbox = document.createElement("input");
         myCheckbox.type = "checkbox";
         var myLabel = document.createElement("label");
         myLabel.className = "myClass";
         myLabel.innerHTML = inputField.value;
+        /*let myTrash: HTMLElement = document.createElement("p");
+        myTrash.className = ("fas fa-trash");
+        myTrash.classList.add("fas fa-trash");*/
         document.getElementById("toDoForm").appendChild(myCheckbox);
         document.getElementById("toDoForm").appendChild(myLabel);
-        var deleteButton = document.createElement("input");
-        deleteButton.className = "fas fa-trash";
+        //document.getElementById("toDoForm").appendChild(myTrash);
     }
 })(L9 || (L9 = {}));
 //# sourceMappingURL=scriptA9.js.map
--- Aufgabe9/scriptA9.ts
+++ Aufgabe9/scriptA9.ts
@@ -4,20 +4,27 @@ const inputField: HTMLInputElement = (document.getElementById("inputField") as H
 
 inputField.addEventListener("keypress", function (event: KeyboardEvent): void {
 if (event.key == "Enter") {
-    createNewToDo();
+    createNewTask();
 }    
 });
 
-function createNewToDo(): void {
+function createNewTask(): void {
     let myCheckbox: HTMLInputElement = document.createElement("input");
     myCheckbox.type = "checkbox";
+
     let myLabel: HTMLLabelElement = document.createElement("label");
     myLabel.className = "myClass";
     myLabel.innerHTML = inputField.value;
+    
+    /*let myTrash: HTMLElement = document.createElement("p");
+    myTrash.className = ("fas fa-trash");
+    myTrash.classList.add("fas fa-trash");*/
+
+
     document.getElementById("toDoForm").appendChild(myCheckbox);
     document.getElementById("toDoForm").appendChild(myLabel);
-    let deleteButton: HTMLInputElement = document.createElement("input");
-    deleteButton.className = "fas fa-trash";
+    //document.getElementById("toDoForm").appendChild(myTrash);
+
 }
 
 
