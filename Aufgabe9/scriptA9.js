51d237d98048558c356460608f9de1a09e3706af
Mariia Kolkutova <kolkutova.mariya@gmail.com>
2020-12-18 11:30:39 +0100

    ???

--- Aufgabe9/scriptA9.js
+++ Aufgabe9/scriptA9.js
@@ -4,19 +4,50 @@ var L9;
     inputField.addEventListener("keypress", function (event) {
         if (event.key == "Enter") {
             createNewTask();
+            inputField.value = " ";
         }
     });
+    var changeCounter = 0;
+    //Funktion für neue To Dos!
     function createNewTask() {
+        //Div
+        var myDiv = document.createElement("div");
+        myDiv.className = "myDiv";
+        //Checkbox
         var myCheckbox = document.createElement("input");
         myCheckbox.type = "checkbox";
+        //Label
         var myLabel = document.createElement("label");
         myLabel.className = "myClass";
         myLabel.innerHTML = inputField.value;
-        var myTrash = document.createElement("p");
+        //Trash
+        var myTrash = document.createElement("i");
         myTrash.className = ("fas fa-trash");
-        document.getElementById("toDoForm").appendChild(myCheckbox);
-        document.getElementById("toDoForm").appendChild(myLabel);
-        document.getElementById("toDoForm").appendChild(myTrash);
+        //?
+        myTrash.addEventListener("click", function () {
+            console.log("trash hehe");
+            removeTask(myDiv);
+        });
+        //getting Ellements --> "child" "appenden" an die Objekte 
+        document.getElementById("toDoFrom").appendChild(myDiv); //????
+        myDiv.appendChild(myCheckbox);
+        myDiv.appendChild(myLabel);
+        myDiv.appendChild(myTrash);
+        console.log();
+        //Task +1 zählen, + open Tasks ausgeben 
+        changeCounter++;
+        countTasks();
+    }
+    //Funktion für das Löschen der ToDOs --> ".remove"
+    function removeTask(wrapper) {
+        wrapper.remove();
+        // Task -1 zählen, nachdem ein div gelöscht wird --> count Tasks angewendet
+        changeCounter--;
+        countTasks();
+    }
+    //Tasks zählen
+    function countTasks() {
+        document.getElementById("taskNumber").innerText = changeCounter + " open tasks";
     }
 })(L9 || (L9 = {}));
 //# sourceMappingURL=scriptA9.js.map
--- Aufgabe9/scriptA9.ts
+++ Aufgabe9/scriptA9.ts
@@ -5,28 +5,62 @@ const inputField: HTMLInputElement = (document.getElementById("inputField") as H
 inputField.addEventListener("keypress", function (event: KeyboardEvent): void {
 if (event.key == "Enter") {
     createNewTask();
+    inputField.value = " ";
 }    
 });
 
+let changeCounter: number = 0;
+
+//Funktion für neue To Dos!
 function createNewTask(): void {
+    //Div
+    let myDiv: HTMLDivElement = document.createElement("div");
+    myDiv.className = "myDiv"; 
+    //Checkbox
     let myCheckbox: HTMLInputElement = document.createElement("input");
     myCheckbox.type = "checkbox";
-
+    //Label
     let myLabel: HTMLLabelElement = document.createElement("label");
     myLabel.className = "myClass";
     myLabel.innerHTML = inputField.value;
-    
-    let myTrash: HTMLElement = document.createElement("p");
+    //Trash
+    let myTrash: HTMLElement = document.createElement("i");
     myTrash.className = ("fas fa-trash");
 
+    //?
+    myTrash.addEventListener("click", function(): void {
+    console.log("trash hehe");
+    removeTask(myDiv);
+    });
 
-
-    document.getElementById("toDoForm").appendChild(myCheckbox);
-    document.getElementById("toDoForm").appendChild(myLabel);
-    document.getElementById("toDoForm").appendChild(myTrash);
+    //getting Ellements --> "child" "appenden" an die Objekte 
+    document.getElementById("toDoFrom").appendChild(myDiv);   //????
+    myDiv.appendChild(myCheckbox);
+    myDiv.appendChild(myLabel);
+    myDiv.appendChild(myTrash);
+    
+    console.log();
+    
+//Task +1 zählen, + open Tasks ausgeben 
+changeCounter++;
+countTasks();
 
 }
 
+//Funktion für das Löschen der ToDOs --> ".remove"
+function removeTask(wrapper: HTMLDivElement): void {
+wrapper.remove(); 
+
+// Task -1 zählen, nachdem ein div gelöscht wird --> count Tasks angewendet
+changeCounter--;
+countTasks();
+
+}
+
+//Tasks zählen
+function countTasks(): void {
+    document.getElementById("taskNumber").innerText = changeCounter + " open tasks";
+}
 
 
 }
